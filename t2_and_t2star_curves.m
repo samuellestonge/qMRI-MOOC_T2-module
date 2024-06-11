a05d8a3122cd21f8e65f679f88034b7b68044125
Samuelle St-Onge <samuellestonge@gmail.com>
2024-06-11 15:53:48 -0400

    update simulation code

--- t2_and_t2star_curves.m
+++ t2_and_t2star_curves.m
@@ -21,10 +21,10 @@ signal_GM_T2star = exp(-params.TE / params.T2star_GM);
 % Plot the T2 and T2* signals
 figure;
 hold on;
-plot(params.TE, signal_WM_T2, '-r', 'DisplayName', 'T2 = 109.77 ms (white matter)');
-plot(params.TE, signal_GM_T2, '-g', 'DisplayName', 'T2 = 96.07 ms (gray matter)');
-plot(params.TE, signal_WM_T2star, '-b', 'DisplayName', 'T2* = 67.63 ms (white matter)');
-plot(params.TE, signal_GM_T2star, '-y', 'DisplayName', 'T2* = 48.48 ms (gray matter)');
+plot(params.TE, signal_WM_T2, '-b', 'DisplayName', 'T2 = 109.77 ms (white matter)');
+plot(params.TE, signal_GM_T2, '-r', 'DisplayName', 'T2 = 96.07 ms (gray matter)');
+plot(params.TE, signal_WM_T2star, '--b', 'DisplayName', 'T2* = 67.63 ms (white matter)');
+plot(params.TE, signal_GM_T2star, '--r', 'DisplayName', 'T2* = 48.48 ms (gray matter)');
 xlabel('Echo Time - TE (ms)');
 ylabel('Transverse Magnetization (Mxy)');
 legend();
