5b147f43ae56c6c70d63a7e1712bfbff72e5a9ab
Samuelle St-Onge <samuellestonge@gmail.com>
2024-06-17 21:48:47 -0400

    update

--- T2w_vs_T2map.m
+++ T2w_vs_T2map.m
@@ -9,7 +9,7 @@ data.Mask = double(load_nii_data('example_data/mono_t2_demo/mono_t2_data/Mask.ni
 % Define fitting parameters
 EchoTime  = [12.8000; 25.6000; 38.4000; 51.2000; 64.0000; 76.8000; 89.6000; 102.4000; 115.2000; 128.0000; 140.8000; 153.6000; 166.4000; 179.2000; 192.0000; 204.8000; 217.6000; 230.4000; 243.2000; 256.0000; 268.8000; 281.6000; 294.4000; 307.2000; 320.0000; 332.8000; 345.6000; 358.4000; 371.2000; 384.0000];
 Model.Prot.SEdata.Mat = [ EchoTime ];
-%FitResults = FitData(data,Model,0); % The '0' flag is so that no wait bar is shown.
+FitResults = FitData(data,Model,0);
 
 % T2w MRI data at different TE values
 TE_1 = imrotate(squeeze(data.SEdata(:,:,:,1).*data.Mask),-90);
@@ -56,8 +56,6 @@ caxis([0, 2500]);
 subplot(2, 3, 4);
 imagesc(TE_4);
 T2_map = imrotate(squeeze(FitResults.T2.*data.Mask),-90);
-xAxis = [0:size(T2_map,2)-1];
-yAxis = [0:size(T2_map,1)-1];
 colormap(gray);
 colorbar;
 axis image;
