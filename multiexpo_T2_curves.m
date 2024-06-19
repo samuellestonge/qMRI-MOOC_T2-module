d2c124d33df25a25ec41161041fa3388b93a30b4
Samuelle St-Onge <samuellestonge@gmail.com>
2024-06-18 21:15:47 -0400

    update

--- multiexpo_T2_curves.m
+++ multiexpo_T2_curves.m
@@ -1,11 +1,25 @@
+% Define initial MWF and T2 times of myelin water and intra- and extracellular water
 x = struct;
-x.MWF = 50.0001;
-x.T2MW = 20.0001;
+x.MWF = 50;
+x.T2MW = 20;
 x.T2IEW = 120;
+
+% Define echo times
+params.TE = linspace(0, 300, 100);
+
 % Set simulation options
-Opt.SNR = 200;
+Opt.SNR = 120;
 Opt.T2Spectrumvariance_Myelin = 5;
 Opt.T2Spectrumvariance_IEIntraExtracellularWater = 20;
-% run simulation
+
+% Run simulation
 figure('Name','Single Voxel Curve Simulation');
 FitResult = Model.Sim_Single_Voxel_Curve(x,Opt);
+
+% T2 relaxation curves for myelin water and intra/extracellular water
+% (using a mono-exponential curve)
+signal_mono_MW = exp(-params.TE / FitResult.T2MW);
+signal_mono_IEW = exp(-params.TE / FitResult.T2IEW);
+
+% T2 relaxation curve for multi-expo model
+signal_multi_MWF = (FitResult.MWF/100)*signal_mono_MW + (1 - FitResult.MWF/100)*signal_mono_IEW;
